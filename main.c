2e7873c822d81e73098a2a00d1fb87bd5abbfeb9
zzatuchni <zzatuchni@outlook.com>
2024-07-22 01:34:52 -0400

    Uart now works

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include <inttypes.h>
 #include <stdbool.h>
+#include <stddef.h>
+
+////////////////////////////////////////////////////////////
+//  Misc
+////////////////////////////////////////////////////////////
+
+#define FREQ 16000000 
+#define BIT(x) (1UL << x)
+
+static inline void spin(uint32_t count) {
+    while (count--) asm("nop");
+}
 
 ////////////////////////////////////////////////////////////
 //  Pin
@@ -14,43 +26,6 @@ typedef struct {
 } STM32_Pin;
 
 ////////////////////////////////////////////////////////////
-//  GPIO
-////////////////////////////////////////////////////////////
-
-#define GPIOA_START_ADDRESS 0x40020000
-#define GPIO_BANK_BOUNDARY_SIZE 0x400
-
-typedef struct {
-    volatile uint32_t 
-        MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFRL, AFRH;
-} GPIO_Bank;
-#define GET_GPIO_BANK(bank) (GPIO_Bank *) ((GPIOA_START_ADDRESS) + (GPIO_BANK_BOUNDARY_SIZE) * ((bank) - 'A'))
-
-typedef enum { 
-    GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG 
-} GPIO_Mode;
-
-typedef uint8_t GPIO_AF;
-
-static inline void gpio_set_mode(GPIO_Mode mode, STM32_Pin pin) {
-    GPIO_Bank *gpio = GET_GPIO_BANK(pin.bank); 
-    int n = pin.number;
-    gpio->MODER &= ~(3U << (n * 2)); //reset mode
-    gpio->MODER |= ((mode & 3U) << (n * 2)); //set to new mode
-}
-
-
-//static inline void gpio_set_af(GPIO_AF af, STM32_Pin pin) {
-//
-//}
-
-static inline void gpio_write(bool value, STM32_Pin pin) {
-    GPIO_Bank *gpio = GET_GPIO_BANK(pin.bank); 
-    int n = pin.number;
-    gpio->BSRR = (1U << n) << (value ? 0 : 16);
-}
-
-////////////////////////////////////////////////////////////
 //  RCC
 ////////////////////////////////////////////////////////////
 
@@ -63,6 +38,55 @@ typedef struct {
         AHB3LPENR, RESERVED4, APB1LPENR, APB2LPENR, RESERVED5[2], BDCR, CSR,
         RESERVED6[2], SSCGR, PLLI2SCFGR;
 } RCC_Regs;
+#define RCC ((RCC_Regs *) RCC_START_ADDRESS)
+
+////////////////////////////////////////////////////////////
+//  GPIO
+////////////////////////////////////////////////////////////
+
+#define GPIOA_START_ADDRESS 0x40020000
+#define GPIO_BANK_BOUNDARY_SIZE 0x400
+
+typedef struct {
+    volatile uint32_t 
+        MODER, OTYPER, OSPEEDR, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
+} GPIO_Bank;
+#define GET_GPIO_BANK(bank) (GPIO_Bank *) ((GPIOA_START_ADDRESS) + (GPIO_BANK_BOUNDARY_SIZE) * ((bank) - 'A'))
+
+typedef enum { 
+    GPIO_MODE_INPUT, GPIO_MODE_OUTPUT, GPIO_MODE_AF, GPIO_MODE_ANALOG 
+} GPIO_Mode;
+
+typedef uint8_t GPIO_AF;
+
+static inline void gpio_set_mode(GPIO_Mode mode, STM32_Pin pin) {
+    GPIO_Bank *gpio = GET_GPIO_BANK(pin.bank); 
+    int n = pin.number;
+    RCC->AHB1ENR |= (1UL << (pin.bank - 'A'));
+    gpio->MODER &= ~(3U << (n * 2));            //reset mode
+    gpio->MODER |= ((mode & 3U) << (n * 2));    //set to new mode
+}
+
+static inline void gpio_set_af(GPIO_AF af, STM32_Pin pin) {
+    GPIO_Bank *gpio = GET_GPIO_BANK(pin.bank); 
+    int n = pin.number;
+    gpio->AFR[n >> 3] &= ~(15UL << ((n & 7) * 4));              //reset af
+    gpio->AFR[n >> 3] |= (((uint32_t) af) << ((n & 7) * 4));     //set to new mode
+}
+
+static inline void gpio_write(bool value, STM32_Pin pin) {
+    GPIO_Bank *gpio = GET_GPIO_BANK(pin.bank); 
+    int n = pin.number;
+    gpio->BSRR = (1U << n) << (value ? 0 : 16);
+}
+
+////////////////////////////////////////////////////////////
+//  LED
+////////////////////////////////////////////////////////////
+
+static inline void led_init(STM32_Pin led_pin) { 
+    gpio_set_mode(GPIO_MODE_OUTPUT, led_pin);
+}
 
 ////////////////////////////////////////////////////////////
 //  Systick
@@ -80,6 +104,7 @@ static inline void systick_init(uint32_t ticks) {
     systick->RVR = ticks - 1;
     systick->CVR = 0;
     systick->CSR |= 7UL;
+    RCC->APB2ENR |= BIT(14);
 }
 
 __attribute__((used)) static volatile uint32_t num_ticks;
@@ -91,38 +116,52 @@ void _on_systick_interrupt(void) {
 //  UART
 ////////////////////////////////////////////////////////////
 
-/*
-#define UART1_START_ADDRESS 0x40011000
+
 #define UART2_START_ADDRESS 0x40004400
-#define UART3_START_ADDRESS 0x40004800
+
+#define UART2_PIN_BANK 'A'
+#define UART2_PIN_NUMBER_TX 2
+#define UART2_PIN_NUMBER_RX 3
+#define UART2_AF_NUMBER 7
+#define UART2_BAUD_RATE 115200
 
 typedef struct {
     volatile uint32_t SR, DR, BRR, CR1, CR2, CR3, GTPR;
 } UART_REGS;
+#define UART2 ((UART_REGS *) UART2_START_ADDRESS)
 
-static inline void uart_init(UART_REGS *uart, uint8_t af_num, uint8_t mode) {
+static inline void uart_init() {
+    RCC->APB1ENR |= BIT(17);
 
+    STM32_Pin uart2_tx_pin = {UART2_PIN_BANK, UART2_PIN_NUMBER_TX};
+    STM32_Pin uart2_rx_pin = {UART2_PIN_BANK, UART2_PIN_NUMBER_RX};
+
+    gpio_set_mode(GPIO_MODE_AF, uart2_tx_pin);
+    gpio_set_mode(GPIO_MODE_AF, uart2_rx_pin);
+
+    gpio_set_af(UART2_AF_NUMBER, uart2_tx_pin);
+    gpio_set_af(UART2_AF_NUMBER, uart2_rx_pin);
+
+    UART2->CR1 = 0;                           // Disable this UART
+    UART2->BRR = FREQ / UART2_BAUD_RATE;      // FREQ is a UART bus frequency
+    UART2->CR1 |= BIT(13) | BIT(2) | BIT(3);  // Set UE, RE, TE
 }
 
-static inline void uart_read_byte() {
-
+static inline uint8_t uart_read_byte() {
+    return (uint8_t) (UART2->DR & 255);
 }
 
-static inline void uart_read_buf() {
-    
+static inline int uart_read_buf() {
+    return (UART2->SR & BIT(5));
 }
 
-static inline void uart_write_byte() {
-
+static inline void uart_write_byte(uint8_t byte) {
+    UART2->DR = byte;
+    while ((UART2->SR & BIT(7)) == 0) spin(1);
 }
 
-static inline void uart_write_buf() {
-    
-}
-*/
-
-static inline void spin(uint32_t count) {
-    while (count--) asm("nop");
+static inline void uart_write_buf(char *buf, size_t len) {
+    while (len-- > 0) uart_write_byte(*(uint8_t *) buf++);
 }
 
 ////////////////////////////////////////////////////////////
@@ -130,29 +169,31 @@ static inline void spin(uint32_t count) {
 ////////////////////////////////////////////////////////////
 
 int main(void) {
-    RCC_Regs *rcc = (RCC_Regs *)RCC_START_ADDRESS;
 
     //  INITIALIZE SYSTICK
-    systick_init(1600);
-    rcc->AHB2ENR |= (1UL << 14);
+    systick_init(FREQ / 1000);
 
     //  INITIALIZE LED
     STM32_Pin led_pin = {LED_PIN_BANK, LED_PIN_NUMBER};
-    rcc->AHB1ENR |= (1UL << (led_pin.bank - 'A')); 
-    gpio_set_mode(GPIO_MODE_OUTPUT, led_pin);
+    led_init(led_pin);
 
-    //for (;;) {}
+    //  INITIALIZE UART
+    uart_init();
+
+    // START LOOP
 
     bool on = true;
     gpio_write(true, led_pin);
     for (;;) {
-        if (on && (num_ticks % 4000) > 2000) {
+        if (on && (num_ticks % 1000) > 500) {
             on = !on;
             gpio_write(on, led_pin);
+            uart_write_buf("hi\r\n", 4);
         }
-        else if (!on && (num_ticks % 4000) < 2000) {
+        else if (!on && (num_ticks % 1000) < 500) {
             on = !on;
             gpio_write(on, led_pin);
+            uart_write_buf("hi\r\n", 4);
         }
 
     }
